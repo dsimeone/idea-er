130cc9540fad5f2b1237f00136f5789377f8fc47
Antonio <geosbook@gmail.com>
2013-10-11 10:55:27 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaEr::Application.config.secret_token = '82b90ed508a2ff89702643643a8267021324e399956db414ad063d682d2bc20295ccff8a40a0d398f523e07d409225c407a0473e4df12f681d7d5c5762d15492'
