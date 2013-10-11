194897d2562d5cf8165a734f536108f75bd44307
Antonio <geosbook@gmail.com>
2013-10-12 00:10:11 +0400

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to IDea ER!"
       redirect_to @user
     else
