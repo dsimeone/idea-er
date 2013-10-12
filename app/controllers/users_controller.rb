1b8e14682b9235d8c56f9b739e40922441ebc598
Antonio <geosbook@gmail.com>
2013-10-12 19:59:50 +0400

    add user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)
   end
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def new
@@ -51,12 +52,6 @@ class UsersController < ApplicationController
 
 
   private
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
